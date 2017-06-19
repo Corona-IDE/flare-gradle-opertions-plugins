64bdf97ce139acdb94483bc91f98f91eb480ef90
Ryan O'Meara <romeara@live.com>
2017-06-19 01:04:26 -0400

    Add basic tests for plugin

--- src/main/groovy/org/starchartlabs/flare/operations/plugin/MergeCoverageReportsPlugin.groovy
+++ src/main/groovy/org/starchartlabs/flare/operations/plugin/MergeCoverageReportsPlugin.groovy
@@ -5,15 +5,17 @@ import org.gradle.api.Project
 import org.gradle.testing.jacoco.tasks.JacocoReport
 import org.starchartlabs.flare.operations.task.MergeCoverageReportsTask
 
-//TODO romeara doc, test
+//TODO romeara doc
 public class MergeCoverageReportsPlugin implements Plugin<Project> {
+
+    private static final String TASK_NAME = 'mergeCoverageReports'
 
     @Override
     public void apply(Project project) {
         project.apply plugin: 'base'
         project.apply plugin: 'jacoco'
 
-        JacocoReport mergeTask = project.getTasks().create('mergeCoverageReport', MergeCoverageReportsTask.class);
+        JacocoReport mergeTask = project.getTasks().create(TASK_NAME, MergeCoverageReportsTask.class);
 
         project.getTasks().getByName('build').dependsOn mergeTask
     }
---
+++ src/test/java/org/starchartlabs/flare/test/operations/plugin/MergeCoverageReportsPluginTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) Jun 19, 2017 Corona IDE.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    "romeara" - initial API and implementation and/or initial documentation
+ */
+package org.starchartlabs.flare.test.operations.plugin;
+
+import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.internal.impldep.org.testng.Assert;
+import org.gradle.testfixtures.ProjectBuilder;
+import org.starchartlabs.flare.operations.task.MergeCoverageReportsTask;
+import org.testng.annotations.Test;
+
+public class MergeCoverageReportsPluginTest {
+
+    private static final String PLUGIN_ID = "org.starchartlabs.flare.merge-coverage-reports";
+
+    @Test
+    public void baseAndJacocoPluginsApplied() throws Exception {
+        Project project = ProjectBuilder.builder().build();
+        project.getPluginManager().apply(PLUGIN_ID);
+
+        Assert.assertTrue(project.getPluginManager().hasPlugin("base"));
+        Assert.assertTrue(project.getPluginManager().hasPlugin("jacoco"));
+    }
+
+    @Test
+    public void taskAddedAndIntegratedWithBuild() {
+        Project project = ProjectBuilder.builder().build();
+        project.getPluginManager().apply(PLUGIN_ID);
+
+        Task task = project.getTasks().getByName("mergeCoverageReports");
+        Assert.assertTrue(task instanceof MergeCoverageReportsTask);
+
+        Task buildTask = project.getTasks().getByName("build");
+
+        Assert.assertTrue(buildTask.getDependsOn().contains(task));
+    }
+
+}
