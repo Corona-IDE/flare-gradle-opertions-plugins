7b116f5fcdf40fac80d5a7880b4af3d47a072121
Ryan O'Meara <romeara@live.com>
2017-06-18 21:45:56 -0400

    Move configuration into a specific task definition

--- src/main/groovy/org/starchartlabs/flare/operations/plugin/MergeCoverageReportsPlugin.groovy
+++ src/main/groovy/org/starchartlabs/flare/operations/plugin/MergeCoverageReportsPlugin.groovy
@@ -3,6 +3,7 @@ package org.starchartlabs.flare.operations.plugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.testing.jacoco.tasks.JacocoReport
+import org.starchartlabs.flare.operations.task.MergeCoverageReportsTask
 
 //TODO romeara doc, test
 public class MergeCoverageReportsPlugin implements Plugin<Project> {
@@ -12,20 +13,7 @@ public class MergeCoverageReportsPlugin implements Plugin<Project> {
         project.apply plugin: 'base'
         project.apply plugin: 'jacoco'
 
-        JacocoReport mergeTask = project.getTasks().create('mergeCoverageReport', JacocoReport.class);
-
-        project.getTasks().getByName('mergeCoverageReport').configure{
-            executionData project.fileTree(project.rootDir.absolutePath).include("**/build/jacoco/*.exec")
-
-            project.subprojects.each { sourceSets it.sourceSets.main }
-
-            reports {
-                xml.enabled true
-                xml.destination "${project.buildDir}/reports/jacoco/report.xml"
-                html.enabled false
-                csv.enabled false
-            }
-        }
+        JacocoReport mergeTask = project.getTasks().create('mergeCoverageReport', MergeCoverageReportsTask.class);
 
         project.getTasks().getByName('build').dependsOn mergeTask
     }
---
+++ src/main/groovy/org/starchartlabs/flare/operations/task/MergeCoverageReportsTask.groovy
@@ -0,0 +1,24 @@
+package org.starchartlabs.flare.operations.task
+
+import org.gradle.testing.jacoco.tasks.JacocoReport
+
+//TODO romeara doc, test
+public class MergeCoverageReportsTask extends JacocoReport {
+
+	public MergeCoverageReportsTask(){
+		super()
+		
+		configure{
+            executionData project.fileTree(project.rootDir.absolutePath).include("**/build/jacoco/*.exec")
+
+            project.subprojects.each { sourceSets it.sourceSets.main }
+
+            reports {
+                xml.enabled true
+                xml.destination "${project.buildDir}/reports/jacoco/report.xml"
+                html.enabled false
+                csv.enabled false
+            }
+        }
+	}
+}
