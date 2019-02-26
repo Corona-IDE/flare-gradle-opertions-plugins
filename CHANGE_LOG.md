135427597b1077835544bff8366f8db61210ec55
Ryan O'Meara <romeara@live.com>
2019-02-25 20:35:27 -0500

    GH-7 Switch from Groovy to Java implementation

--- CHANGE_LOG.md
+++ CHANGE_LOG.md
@@ -5,12 +5,18 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
+### Changed
+- (GH-7) Switch from Groovy to Java implementation
 
 ## [0.2.0]
+### Added
 - (GH-10) Add "dependencProjectReport" task to allow finding dependency instances specifically of projects in the same multi-module configuration
+
+### Changed
 - (GH-9) Switch to passing report location as a file to try and use newer APIs when available
 
 ## [0.1.0]
+### Added
 - Create a plug-in which adds standard behavior for creating a merged code coverage report
 - Create a plug-in which adds standard conventions for test logging
 - Create a plug-in which adds standard dependency information tasks
--- src/main/java/org/starchartlabs/flare/operations/dsl/ProjectDependencyResultSpec.java
+++ src/main/java/org/starchartlabs/flare/operations/dsl/ProjectDependencyResultSpec.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Aug 23, 2018 StarChart Labs Authors.
+ * Copyright (c) Feb 25, 2019 StarChart Labs Authors.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,6 +10,9 @@
  */
 package org.starchartlabs.flare.operations.dsl;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.ModuleVersionIdentifier;
 import org.gradle.api.artifacts.component.ComponentSelector;
@@ -19,29 +22,30 @@ import org.gradle.api.artifacts.result.ResolvedDependencyResult;
 import org.gradle.api.specs.Spec;
 
 /**
- * Dependency matching specification which is designed to only match against other projects/modules in the same multi-module system as the provided project
+ * Dependency matching specification which is designed to only match against other projects/modules in the same
+ * multi-module system as the provided project
  *
  * <p>
- * Heavily based on default Gradle <a href="https://github.com/gradle/gradle/blob/master/subprojects/diagnostics/src/main/java/org/gradle/api/tasks/diagnostics/internal/dsl/DependencyResultSpec.java">"DependencyResultSpec"</a>
+ * Heavily based on default Gradle <a href=
+ * "https://github.com/gradle/gradle/blob/master/subprojects/diagnostics/src/main/java/org/gradle/api/tasks/diagnostics/internal/dsl/DependencyResultSpec.java">"DependencyResultSpec"</a>
  *
  * @author romeara
  * @since 0.2.0
  */
 public class ProjectDependencyResultSpec implements Spec<DependencyResult> {
 
-    private final Set<String> projectIdentifiers
+    private final Set<String> projectIdentifiers;
 
-    private final Project project
+    private final Project project;
 
     public ProjectDependencyResultSpec(Project project) {
-        this.project = project
+        this.project = project;
         projectIdentifiers = new HashSet<>();
 
-        project.rootProject.allprojects{p ->
-            projectIdentifiers.add(p.path)
-
-            project.getLogger().debug("Adding path ${p.path} to search configuration of project ${project.path}")
-        }
+        project.getRootProject().getAllprojects().forEach(p -> {
+            projectIdentifiers.add(p.getPath());
+            project.getLogger().debug("Adding path ${p.path} to search configuration of project ${project.path}");
+        });
     }
 
     @Override
@@ -57,12 +61,14 @@ public class ProjectDependencyResultSpec implements Spec<DependencyResult> {
         ComponentSelector requested = candidate.getRequested();
 
         if (requested instanceof ModuleComponentSelector) {
-            ModuleComponentSelector requestedModule = (ModuleComponentSelector) requested
-            String requestedCandidate = "${requestedModule.group}:${requestedModule.module}:${requestedModule.version}"
+            ModuleComponentSelector requestedModule = (ModuleComponentSelector) requested;
+            String requestedCandidate = requestedModule.getGroup() + ":" + requestedModule.getModule() + ":"
+                    + requestedModule.getVersion();
 
-            project.getLogger().debug("Comparing DependencyResult ${requestedCandidate} to known projects")
+            project.getLogger().debug("Comparing DependencyResult ${requestedCandidate} to known projects");
 
-            return projectIdentifiers.any{ p -> requestedCandidate.contains(p) }
+            return projectIdentifiers.stream()
+                    .anyMatch(requestedCandidate::contains);
         }
 
         return false;
@@ -70,10 +76,11 @@ public class ProjectDependencyResultSpec implements Spec<DependencyResult> {
 
     private boolean matchesSelected(ResolvedDependencyResult candidate) {
         ModuleVersionIdentifier selected = candidate.getSelected().getModuleVersion();
-        String selectedCandidate = "${selected.group}:${selected.name}:${selected.version}"
+        String selectedCandidate = selected.getGroup() + ":" + selected.getModule() + ":" + selected.getVersion();
 
-        project.getLogger().debug("Comparing ResolvedDependencyResult ${selectedCandidate}  to known projects")
+        project.getLogger().debug("Comparing ResolvedDependencyResult ${selectedCandidate}  to known projects");
 
-        return projectIdentifiers.any{ p -> selectedCandidate.contains(p) }
+        return projectIdentifiers.stream()
+                .anyMatch(selectedCandidate::contains);
     }
 }
